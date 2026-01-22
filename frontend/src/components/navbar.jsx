9fb776b7445bd21e10fd28bdabd60a59728f7339
Uday Kumar Reddy <udaykumarreddy939@gmail.com>
2026-01-22 11:42:20 +0530

    created first navbar component

---
+++ frontend/src/components/navbar.jsx
@@ -0,0 +1,28 @@
+import "./navbar.css";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="logo">Codes Club</div>
+      <ul className="links">
+        <li>
+          <a href="/">About</a>
+        </li>
+        <li>
+          <a href="pillars">Pillars</a>
+        </li>
+        <li>
+          <a href="impact">Impact</a>
+        </li>
+        <li>
+          <a href="projects">Projects</a>
+        </li>
+      </ul>
+      <div>
+        <button className="login">Login</button>
+        <button className="join">Join</button>
+      </div>
+    </nav>
+  );
+};
+export default Navbar;
